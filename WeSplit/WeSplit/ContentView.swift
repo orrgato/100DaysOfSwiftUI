2b0422c6440ee9c3b45bc906f2390202c1086b8e
shiwam <vermashivams1510@gmail.com>
2022-04-07 13:49:09 +0530

    Converto commit

--- WeSplit/WeSplit/ContentView.swift
+++ WeSplit/WeSplit/ContentView.swift
@@ -8,9 +8,92 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var checkAmount = 0.0
+    @State private var numberOfPeople = 2
+    @State private var tipPercentage = 20
+    
+    @FocusState private var amountIsFocused: Bool
+
+    let tipPercentages = [10, 15, 20, 25, 0]
+    
+    var totalAmountWithTip: Double {
+        let tipSelection = Double(tipPercentage)
+        let tipValue = checkAmount / 100 * tipSelection
+        let totalAmount = checkAmount + tipValue
+
+        return totalAmount
+    }
+    var totalPerPerson: Double {
+        let peopleCount = Double(numberOfPeople + 2)
+        let tipSelection = Double(tipPercentage)
+        
+        let tipValue = checkAmount / 100 * tipSelection
+        let totalAmount = checkAmount + tipValue
+        let amountPerPerson = totalAmount/peopleCount
+        
+        return amountPerPerson
+    }
+    
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            Form {
+                Section {
+                    TextField("Amount", value: $checkAmount, format: .currency(code: Locale.current.currencyCode ?? "USD"))
+                        .keyboardType(.decimalPad)
+                        .focused($amountIsFocused)
+                    
+                    Picker("Number Of People", selection: $numberOfPeople) {
+                        ForEach(2..<100) {
+                            Text("\($0) People")
+                        }
+                    }
+                } header: {
+                    Text("CheckAmount and total number of people")
+                }
+                
+                Section {
+//                    Text("Select tip percent")
+                    
+                    Picker("Tip percentage", selection: $tipPercentage) {
+                        ForEach(0..<101) {
+                            Text($0, format: .percent)
+                        }
+                    }
+//                    .pickerStyle(.segmented)
+                } header: {
+                    Text("How much tip do you want to leave.?")
+                }
+                
+                Section {
+                    Group {
+                        Text(totalAmountWithTip,format: .currency(code: Locale.current.currencyCode ?? "USD"))
+                    }
+                } header: {
+                    Text("Amount with tip")
+                }
+                
+                Section {
+                    Group {
+//                        Text(checkAmount, format: .currency(code: Locale.current.currencyCode ?? "USD"))
+                        Text(totalPerPerson, format: .currency(code: Locale.current.currencyCode ?? "USD"))
+//                        Text("\(totalPerPerson)")
+                    }
+                } header: {
+                    Text("Amount per person")
+                }
+            }
+            .navigationTitle("WeSplit")
+            .toolbar {
+                ToolbarItemGroup(placement: .keyboard) {
+                    Spacer()
+                    
+                    Button("Done") {
+                        amountIsFocused = false
+                    }
+                }
+            }
+        }
     }
 }
 
---
+++ converto/converto/convertoApp.swift
@@ -0,0 +1,17 @@
+//
+//  convertoApp.swift
+//  converto
+//
+//  Created by Shiwam verma on 07/04/22.
+//
+
+import SwiftUI
+
+@main
+struct convertoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
